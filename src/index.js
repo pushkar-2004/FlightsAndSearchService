0c95cd9e1578a3d34ca3493341510837fd5ab424
Pushkar Kumar <qwertypushkar123@gmail.com>
2025-12-30 22:48:26 +0530

    created databse using sequelite

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
     const app = express();
-    const PORT = 3000;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
     app.listen(PORT , ()=>{
         console.log(`Server started on PORT ${PORT}`);
     });
