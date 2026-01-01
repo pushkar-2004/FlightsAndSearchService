431a5871fb4851c2cabb87ed13e082a3b4c9e591
Pushkar Kumar <qwertypushkar123@gmail.com>
2026-01-01 20:01:58 +0530

    created first route

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,82 @@
+const {CityService} = require('../services/index');
+
+const cityService = new CityService();
+
+const create = async (req , res) => {
+    try{
+        const city = await cityService.createCity(req.body);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:"successfully created city",
+        });
+    }catch(err){
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:"not able to create a city",
+            err:err,
+        });
+    }
+}
+
+const destroy = async (req,res) => {
+    try {
+        const city = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data:city,
+            success:true,
+            message:"successfully created city",
+            err:{}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:"not able to delete a city",
+            err:err,
+        });
+    }
+}
+
+
+// GET -> /city/:id
+const get = async (req,res) => {
+    try {
+        const city = await cityService.getCity(req.params.id);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:"successfully created city",
+            err:{}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:"not able to delete a city",
+            err:err,
+        });
+    }
+}
+
+
+// PATCH  -> /city/:id , req.body
+const update = async (req,res) => {
+    try {
+        const city = await cityService.updateCity(req.params.id,req.body);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:"successfully created city",
+            err:{}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:"not able to delete a city",
+            err:err,
+        });
+    }
+}
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -1,1 +1,56 @@
 const {CityRepository} = require('../repository/index'); 
+
+class CityRepository{
+    constructor(){
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(data){
+
+        try{
+            const city = new this.cityRepository.createCity(data);
+            return city;
+        }catch(err){
+            console.log("Something went wrong inside the city serivce create city");
+            throw {err};
+        }
+
+    };
+
+    async deleteCity(cityId) {
+
+        try{
+            const city = new this.cityRepository.deleteCity(cityId);
+            return city;
+        }catch(err){
+            console.log("Something went wrong inside the city serivce create city");
+            throw {err};
+        }
+
+    };
+
+    async updateCity(cityId,data) {
+
+        try{
+            const city = new this.cityRepository.updateCity(cityId,data);
+            return city;
+        }catch(err){
+            console.log("Something went wrong inside the city serivce create city");
+            throw {err};
+        }
+
+    };
+
+    async getCity(cityId) {
+
+        try{
+            const city = new this.cityRepository.getCity(cityId);
+            return city;
+        }catch(err){
+            console.log("Something went wrong inside the city serivce create city");
+            throw {err};
+        }
+
+    };
+
+}
